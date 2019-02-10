12e0d94fdcafd333f819993eae0cb3cd9ad8155c
Unknown <emma.durand@ynov.com>
2019-02-10 18:16:55 +0100

    problème option -kikoo

--- index.py
+++ index.py
@@ -4,35 +4,52 @@ import markdown2
 
 #fonction permettant de convertir le .md en .html
 def convert(md_fichier, html_fichier):
-    input_file = open(md_fichier, "r", encoding="UTF-8")
+    input_file = open(md_fichier, "r", encoding="utf-8")
     text = input_file.read()
     html = markdown2.markdown(text)
-    output_file = open(html_fichier, "w",encoding="UTF-8")
+    output_file = open(html_fichier, "w",encoding="utf-8")
     output_file.write(html) 
     output_file.close()
     print('Conversion .md en .html fait avec succès')
 
 #fonction permettant de convertir le .md en .html version allemande
 def aide_allemands(md_fichier, html_fichier):
-    input_file = open(md_fichier, "r",encoding="UTF-8")
+    input_file = open(md_fichier, "r",encoding="utf-8")
     text = input_file.read()
     html = markdown2.markdown(text)
-    output_file = open(html_fichier, "w",encoding="UTF-8")
+    output_file = open(html_fichier, "w",encoding="utf-8")
     output_file.write(html) 
     output_file.close()
-    sansallemands = open(html_fichier, "r",encoding="UTF-8")
+    sansallemands = open(html_fichier, "r",encoding="utf-8")
     text2 = sansallemands.read()
-    avecallemands = open(html_fichier, "w",encoding="UTF-8")
+    avecallemands = open(html_fichier, "w",encoding="utf-8")
     aide = text2.replace("ss", "z").replace("s", "z").replace("qu", "k").replace("ce", "ze").replace("c", "k").replace("ç", "z").replace("ph", "f").replace("pp", "p").replace("gu", "ch").replace("g", "ch").replace("j", "k").replace("v", "f").replace("s", "").replace("mm","m")
     avecallemands.write(aide)
     avecallemands.close()
     print("Conversion .md en .html version allemande réussite.")
+
+#def kikoo(md_fichier, html_fichier):
+#    input_file = open(md_fichier, "r",encoding="utf-8")
+#    text = input_file.read()
+#    html = markdown2.markdown(text)
+#    output_file = open(html_fichier, "w",encoding="utf-8")
+#    output_file.write(html) 
+#    output_file.close()
+#    sanskikoo = open(html_fichier, "r", encoding="utf-8")
+#    text2 = sanskikoo.read()
+#    aveckikoo = open(html_fichier, "w", encoding="utf-8")
+#    ajoutkikoo = html_fichier.append(["<p>Kikoo</p>", "<p>lol</p>", "<p>mdr</p>", "<p>ptdr</p>", "<p>lolilo</p>"])
+#    aveckikoo.write(ajoutkikoo)
+#    aveckikoo.close()
+#    print("ajout de kikoo réussit")
+
 
 #ajout d'option à l'utilisation du convertisseur
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", '--input',help='Chemin vers le fichier .md')
 parser.add_argument("-o", '--output',help='Chemin vers le fichier .html')
 parser.add_argument("-a", '--achtung',help='Convertir texte pour faciliter les allemands', action="store_true")
+#parser.add_argument("-k", '--kikoo',help="ajoute 'kikoo', 'lol', 'ptdr'... dans votre page html", action="store_true")
 args = parser.parse_args()
 
 #éxecution du convertisseur selon les options choisies
@@ -40,6 +57,10 @@ if args.achtung is True:
     print('Convertisseur .md en .html')
     print("Pour plus de renseignement sur l'utilisation du convertisseur, veuillez lire le README")
     aide_allemands(args.input,args.output)
+#elif args.kikoo is True :
+#    print('Convertisseur .md en .html')
+#    print("Pour plus de renseignement sur l'utilisation du convertisseur, veuillez lire le README")
+#    kikoo(args.input, args.output)
 else:
     print('Convertisseur .md en .html')
     print("Pour plus de renseignement sur l'utilisation du convertisseur, veuillez lire le README")
